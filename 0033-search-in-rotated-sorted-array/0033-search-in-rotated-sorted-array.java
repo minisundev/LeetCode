d891ecb6491a633b51a2c98b9b774891ad77f51c
Minsun <58019747+minisundev@users.noreply.github.com>
2024-08-04 11:59:54 +0900

    Time: 9 ms (100%), Space: 42.3 MB (16.16%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,48 +1,54 @@
 class Solution {
     public int search(int[] nums, int target) {
-
-        if(nums.length==1){
-            if(nums[0]==target){
-                return 0;
-            }
-            else{
-                return -1;
-            }
-        }
-
-        int pivot = 0;
-        int standard = nums[0];
+        
+        int pivot = -1;
 
         int low = 0;
-        int high = nums.length -1;
+        int high = nums.length-1;
+
+        int standard = nums[0];
+
+        //lowerbound
+        while(low<high){
+            int mid = (low+high)/2;
+
+            //올려라, 같거나 (0에 있으면 0...)크면
+            if(nums[mid]>=standard){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        pivot = low;
+        System.out.println(pivot);
+
+        low = 0;
+        high = nums.length-1;
 
         while(low<=high){
-            pivot = (low+high)/2;
+            int mid = (low+high)/2;
+            int realmid = (mid+pivot)%nums.length;
 
-            int num = nums[pivot];
-
-            if(standard < num){
-                low = pivot+1;
+            if(nums[realmid]>target){
+                high = mid-1;
+            }else if(nums[realmid]<target){
+                low = mid+1;
             }else{
-                high = pivot-1;
+                return realmid;
             }
         }
 
-        low = 0;
-        high = nums.length -1;
+        System.out.println("low "+low+"high "+high);
 
-        while(low <= high){
-            int mid = ((low+high)/2+pivot)%(nums.length -1);
-
-            int num = nums[mid];
-
-            if(num > target){
-                high = mid -1;
-            }else if(num < target){
-                low = mid + 1;
-            }else{
-                return mid;
+        try{
+            if(nums[low%nums.length]==target){
+                return low;
             }
+            if(nums[high%nums.length]==target){
+                return high;
+            }
+        }catch(Exception e){
+            return -1;
         }
         return -1;
     }
