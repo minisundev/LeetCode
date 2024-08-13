328a7dd4dab98b2a691780c87b7e38243bc08b42
Minsun <58019747+minisundev@users.noreply.github.com>
2024-08-13 23:08:53 +0900

    Time: 70 ms (5%), Space: 44.6 MB (6.61%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,34 @@
+import java.lang.Math;
+
+class Solution {
+    public int mySqrt(int x) {
+        if(x==1){
+            return 1;
+        }
+
+        int low = 1;
+        int high = x/2;
+        int max = 0;
+
+        //upperbound
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            long square = (long)mid*mid;
+
+            System.out.println(low+"-"+high+" "+mid+" : "+square);
+            
+            
+            if(square<x){//작거나 같으면 올려라
+                low = mid+1;
+                 max = Math.max(max,mid);
+            }else if(square>x){
+                high = mid-1;
+            }else{//같으면 넣고 나가라....
+                max = mid;
+                break;
+            }
+        }
+        return max;
+    }
+}
