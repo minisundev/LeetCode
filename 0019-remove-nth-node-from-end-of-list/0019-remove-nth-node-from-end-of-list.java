b1b4f0118a1214ae1484343521a59bb703a2ad0a
Minsun <58019747+minisundev@users.noreply.github.com>
2024-08-23 22:35:57 +0900

    Time: 0 ms (100%), Space: 42.1 MB (8.12%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,22 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+import java.util.*;
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(0);//edge case 처리 쉽게 하기
         dummy.next = head;
 
-        ListNode first = dummy;
-        ListNode second = dummy;
+        ListNode nBefore = dummy;
+        ListNode now = dummy;
 
-        for (int i = 1; i <= n + 1; i++) {
-            first = first.next;
+        for(int i=0; i<n; i++){
+            if(now == null) return head;
+            now = now.next;
         }
 
-        while (first != null) {
-            first = first.next;
-            second = second.next;
+        while(now.next!=null){
+            nBefore = nBefore.next;
+            now = now.next;
         }
 
-        second.next = second.next.next;
+        nBefore.next = nBefore.next.next;
 
-        return dummy.next;
+        return dummy.next;//head이면 안됨 그게 삭제되었을수도 있어
     }
 }
