48190b05885b865e3e77e3042f83899966d3fdf7
Minsun <58019747+minisundev@users.noreply.github.com>
2024-09-18 23:59:20 +0900

    Time: 1 ms (100%), Space: 48.1 MB (76.63%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,16 +1,25 @@
-class Solution {
-    public List<Integer> lexicalOrder(int n) {
-        List<Integer> answer = new ArrayList<>();
-        for(int i=1; i<=n; i++){
-            answer.add(i);
+import java.util.*;
+
+public class Solution {
+    
+    public List<Integer> lexicalOrder(int limit) {
+        List<Integer> result = new ArrayList<>(limit); 
+        for (int i = 1; i <= 9; i++) { 
+            generateLexicalNumbers(i, limit, result);
         }
+        return result;
+    }
 
-        Collections.sort(answer,
-        (o1,o2)->
-        {
-            return Integer.toString(o1).compareTo(Integer.toString(o2));
-        });
+    private void generateLexicalNumbers(int currentNumber, int limit, List<Integer> result) {
+        if (currentNumber > limit) return;
+        result.add(currentNumber);
 
-        return answer;
+        for (int nextDigit = 0; nextDigit <= 9; ++nextDigit) {
+            int nextNumber = currentNumber * 10 + nextDigit;
+            
+            if (nextNumber > limit) break;
+            
+            generateLexicalNumbers(nextNumber, limit, result);
+        }
     }
 }
