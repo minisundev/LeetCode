bb8af968912bfc9d2d4e536d8592eb64618eb31a
Minsun <58019747+minisundev@users.noreply.github.com>
2024-10-08 22:56:57 +0900

    Time: 32 ms (73.77%), Space: 41.4 MB (99.24%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,7 +5,6 @@ class Solution {
     boolean result=false;
 
     public boolean canPartition(int[] nums) {
-        //백트래킹으로 풀어보기
         int sum = 0;
         arr = nums;
 
@@ -22,9 +21,7 @@ class Solution {
 
         boolean[] dp = new boolean[S + 1];
 
-        for (int i = 0; i < N; i++) {
-            dp[0] = true;//공집합
-        }
+        dp[0] = true;//공집합
 
         for (int i = 1; i < N; i++) {
             for (int s = S; s >= nums[i] ; s--) {//현재 원소를 포함할 수 있는 경우 모두 체크
