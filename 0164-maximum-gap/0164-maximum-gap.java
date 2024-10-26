7cf58475285c7e3d2cb8a1844ca2cca7e05855cb
Minsun <58019747+minisundev@users.noreply.github.com>
2024-10-26 15:05:02 +0900

    Time: 8 ms (99.84%), Space: 57.1 MB (96.92%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -7,16 +7,52 @@ class Solution {
             return 0;
         }
 
-        Arrays.sort(nums);//정렬 안 하고 O(n)만으로 어떻게 풀 수 있는게 있나...
-
-        int last = nums[0];
-        int max = Math.abs(nums[1]-nums[0]);
-
-        for(int i=2;i<nums.length;i++){
-            int gap = Math.abs(nums[i]-nums[i-1]);
-            max = Math.max(max,gap);
+        if(nums.length<=2){
+            return Math.abs(nums[0]-nums[1]);
         }
 
-        return max;
+        //정렬 안 하고 O(n)만으로 풀기 위해 버킷을 도입
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int num : nums){
+            max = Math.max(max,num);
+            min = Math.min(min,num);
+        }
+
+        int gap = (int)Math.ceil((double)(max-min)/(double)(nums.length-1));
+        //갭의 최소단위는 최대와 최소의 차이를 갯수만큼 나눈 값이겠지
+
+        if(gap==0){//모든 값이 동일할 때
+            return 0;
+        }
+
+        int bucketNumber = nums.length;
+        int [] minBucket = new int [bucketNumber];
+        int [] maxBucket = new int [bucketNumber];
+
+        for(int i=0;i<bucketNumber;i++){
+            minBucket[i] = Integer.MAX_VALUE;
+            maxBucket[i] = Integer.MIN_VALUE;
+        }
+
+        for(int num: nums){
+            int index = (num-min)/gap;//얘가 들어갈 자리를 찾아보자~~
+            minBucket[index] = Math.min(minBucket[index],num);
+            maxBucket[index] = Math.max(maxBucket[index],num);
+        }
+
+        int maxGap = 0;
+        int prevMax = min;
+
+        for(int i=0;i<bucketNumber;i++){
+            if (minBucket[i] == Integer.MAX_VALUE) {
+                continue; // 버킷이 비어 있으면 건너뜁니다
+            }
+            maxGap = Math.max(maxGap, minBucket[i] - prevMax);
+            prevMax = maxBucket[i];
+        }
+
+        return maxGap;
     }
 }
