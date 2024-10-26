2e08c3203ba5091b34e73c42369be5966008d8e9
Minsun <58019747+minisundev@users.noreply.github.com>
2024-10-26 13:46:53 +0900

    Time: 4 ms (23.13%), Space: 42.6 MB (18.76%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,50 +1,47 @@
 class Solution {
     public String addBinary(String a, String b) {
-        String result = "";
+
+        //a가 더 길다고 가정
+        if(b.length()>a.length()){
+            return addBinary(b,a);
+        }
+        char [] arr = new char[a.length()];
+
         int aIdx = a.length()-1;
-        int bIdx = b.length()-1;
         int increment = 0;
 
-        while(aIdx>=0||bIdx>=0){
-            if(aIdx<0){
-                int number = (b.charAt(bIdx)-'0')+increment;
-                if(number>=2){
-                    increment = 1;
-                    number = number-2;
-                }else{
-                    increment = 0;
-                }
-                System.out.println(number+","+increment);
-                result = number+result;
-                bIdx--;
-            }else if(bIdx<0){
-                int number = (a.charAt(aIdx)-'0')+increment;
-                if(number>=2){
-                    increment = 1;
-                    number = number-2;
-                }else{
-                    increment = 0;
-                }
-                System.out.println(number+","+increment);
-                result = number+result;
-                aIdx--;
+        for(int bIdx = b.length()-1; bIdx>=0; bIdx--){
+            int number = (b.charAt(bIdx)-'0')+(a.charAt(aIdx)-'0')+increment;
+            if(number>=2){
+                increment = 1;
+                number = number-2;
             }else{
-                int number = (b.charAt(bIdx)-'0')+(a.charAt(aIdx)-'0')+increment;
-                if(number>=2){
-                    increment = 1;
-                    number = number-2;
-                }else{
-                    increment = 0;
-                }
-                System.out.println(number+","+increment);
-                result = number+result;
-                aIdx--;
-                bIdx--;
+                increment = 0;
             }
+            //System.out.println(number+","+increment);
+            arr[aIdx] = (char)(number+'0');
+            aIdx--;
         }
+
+        while(aIdx>=0){
+            int number = (a.charAt(aIdx)-'0')+increment;
+            if(number>=2){
+                increment = 1;
+                number = number-2;
+            }else{
+                increment = 0;
+            }
+            //System.out.println(number+","+increment);
+            arr[aIdx] = (char)(number+'0');
+            aIdx--;
+        }
+
+        String result = new String(arr);
+
         if(increment==1){
             result = increment+result;
         }
+
         return result;
     }
 }
