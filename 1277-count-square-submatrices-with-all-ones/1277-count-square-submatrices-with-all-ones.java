b01abd75b58bb48620ae4b3e460c3ea39415c82e
Minsun <58019747+minisundev@users.noreply.github.com>
2024-11-13 18:47:10 +0900

    Time: 733 ms (5.7%), Space: 56.5 MB (7.91%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -15,8 +15,10 @@ class Solution {
 
         for(int i=1; i<dp.length; i++){
             for(int j=1; j<dp[0].length; j++){
-                dp[i][j] = Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+matrix[i][j];
-                count+=dp[i][j];
+                if(matrix[i][j]!=0){
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+matrix[i][j];
+                    count+=dp[i][j];
+                }
                 System.out.println(i+","+j+":"+dp[i][j]);
             }
         }
