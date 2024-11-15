bb63b0abd14cd8f704b2faa44e81f1b75be63392
Minsun <58019747+minisundev@users.noreply.github.com>
2024-11-15 18:55:09 +0900

    Time: 917 ms (5.1%), Space: 42.1 MB (38.84%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -2,11 +2,24 @@ class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
 
         int [] dp = new int [arr.length+1];
+        List<Node> list = new ArrayList<Node>();
         
         for(int i=0;i<dp.length;i++){
+            if(i<arr.length){
+                list.add(new Node(i,arr[i]));
+            }
+
+            Node n;
+            for(int j=list.size()-1; j>=0; j--){
+                n= list.get(j);
+                if(n.index<i-k){
+                    list.remove(j);
+                }
+            }
+
             for(int j=1; j<=k; j++){
                 if(i-j>=0){
-                    dp[i] = Math.max(dp[i],dp[i-j]+j*findMax(i-j,i-1,arr));
+                    dp[i] = Math.max(dp[i],dp[i-j]+j*findMax(i-j,i-1,list));
                 }
             }
         }
@@ -21,5 +34,26 @@ class Solution {
             max=Math.max(max,arr[i]);
         }
         return max;
+    }
+
+    int findMax(int start, int end, List<Node> list){
+        int max = 0;
+        for(int i=list.size()-1; i>=0; i--){
+            Node n = list.get(i);
+            if(n.index>=start && n.index<=end){
+                max=Math.max(max,n.value);
+            }
+        }
+        return max;
+    }
+}
+class Node{
+    public int index;
+    public int value;
+    public Node (
+        int i, int v
+    ){
+        index = i;
+        value = v;
     }
 }
