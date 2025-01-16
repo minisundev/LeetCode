0720fc60309a01a00fea8e9bd55965263fbf3d04
Minsun <58019747+minisundev@users.noreply.github.com>
2025-01-17 08:28:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] childs, int[] cookies) {
+        if(cookies.length==0) return 0;
+        int count = 0;
+        int child = childs.length-1;
+        int cookie = cookies.length-1;
+
+        while(child>=0 && cookie>=0){
+            if(cookies[cookie]>=childs[child]){
+                count++;
+                cookie--;
+                child--;
+            }else{//childs[child]<cookies[cookie]
+                child--;
+            }
+        }
+
+        return count;
+
+    }
+}
