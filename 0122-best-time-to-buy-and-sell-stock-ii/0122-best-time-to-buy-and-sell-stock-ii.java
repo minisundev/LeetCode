6eb28007c67ba5308b3d19f77bda9acc8d15f876
Minsun <58019747+minisundev@users.noreply.github.com>
2025-01-25 12:19:34 +0900

    Time: 1 ms (86.94%), Space: 45.7 MB (69.77%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,19 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        //int [] min = new int [prices.length];
         int min = prices[0];
-        int max = prices[0];
-        int profit = 0;
-
-        //보다보니까 최대 이익은 그냥 사이가 +가 될때마다 샀다가 팔았다가 할 때 나옴 -> 빗물 채우는 문제랑 비슷한 거 아니냐며   
-
-        for(int i=1; i<prices.length; i++){
-            if(prices[i]>min){
-                profit+=prices[i]-min;
+        
+        for(int i=1;i<prices.length;i++){
+            //근데 지금 보니까 내릴때마다 무조건 사고 오를때마다 무조건 파는게 최대수익이 나와 단타로
+            //더 홀딩했다가 파는것도 의미없는게 계속 오르는 중일때도 무조건 팔고 거기서 또 매수해서 더 올랐을때 또 무조건 팔면 되는거라 그냥 내리면 사고 오르면 팔면 최대수익임
+            //내려가는동안 내려가는것의 최저를 dp에 기록 -> 올라가면 팔면서 매입하고 또 팔기
+            if(min>prices[i]){
                 min = prices[i];
             }else{
+                maxProfit += prices[i]-min;
                 min = prices[i];
             }
         }
-        return profit;
+        return maxProfit;
     }
 }
