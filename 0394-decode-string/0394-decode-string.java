8674d7c4d2ad4ae1a0416459b1d6632392465ffd
Minsun <58019747+minisundev@users.noreply.github.com>
2025-02-02 23:52:52 +0900

    Time: 1 ms (74.29%), Space: 41.2 MB (96.34%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,14 +1,11 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Repeat> stack = new Stack();
-        // [ ] 괄호 사이를 처음 만나는 순간 가장 안쪽의 괄호부터 저장했다가 뽑아서 연산해야 해
-        // 아 아무것도 풀리는게 없는데 이정도는 풀고 내리고 싶어...
         // 이거 그냥 가져다가 쓰면 안되나..? [ ]이 없을때까지 반복해서 해주하면 될 거 같은데 그냥...
         // 3[a2[c]] -> a2[c]a2[c]a2[c] -> accaccacc
         while (s.contains("[")) {
 
-        StringBuilder number = new StringBuilder();
-        StringBuilder newString = new StringBuilder();
+            StringBuilder number = new StringBuilder();
+            StringBuilder newString = new StringBuilder();
 
             for (int i = 0; i < s.length(); i++) {
                 char c = s.charAt(i);
@@ -27,12 +24,12 @@ class Solution {
                         end++;
                     }
 
-                    String target = s.substring(i+1,end-1);
+                    String target = s.substring(i + 1, end - 1);
                     newString.append(target.repeat(Integer.parseInt(number.toString())));
 
-                    i = end-1;
+                    i = end - 1;
                     number = new StringBuilder();
-                }else{
+                } else {
                     newString.append(c);
                 }
             }
@@ -44,18 +41,4 @@ class Solution {
 
     }
 
-}
-
-class Repeat {
-    // public String pre;
-    public String s;
-    public int i;
-
-    // public String post;
-    public Repeat(String s, int i /* , String post,String pre */) {
-        // this.pre = pre;
-        this.s = s;
-        this.i = i;
-        // this.post = post;
-    }
 }
