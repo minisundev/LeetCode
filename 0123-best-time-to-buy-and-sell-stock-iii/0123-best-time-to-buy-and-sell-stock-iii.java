29bd4c244917f535d99d913d5bafcd2cf816458a
Minsun <58019747+minisundev@users.noreply.github.com>
2025-02-13 08:10:57 +0900

    Time: 5 ms (53.53%), Space: 61.4 MB (53.12%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,35 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int max = 0;
-        int [] left = new int [prices.length];//min
-        int [] dp = new int [prices.length];
-        int [] right = new int [prices.length];//max
-        //앞에서부터 판 수익이랑 뒤에서부터 판 수익이랑 더했을때 최고값 나오는거 구하면 되지 않을까 싶음!
+        //state 정의 : hold => sold => hold2 => rest
+        int [] hold = new int [prices.length];
+        int [] sold = new int [prices.length];
+        int [] hold2 = new int [prices.length];
 
-        //왼쪽부터 여태까지 가장 작았던 것 기록
-        // -> 으로 일단 한 번 돌렸을때의 최댓값을 구함
-
-        //오른쪽부터 여태까지 가장 큰 것 기록
-        //을 합하던가... 왼쪽수익만으로 비교하던가... 해서 MAX값을 찾아서 돌려보내면 되지 않을까요 ㅎ
-        left[0] = prices[0];//min
-        dp[0] = 0;
+        hold[0] = -prices[0];//처음에 사버려~~~
+        sold[0] = 0;
+        hold2[0] = Integer.MIN_VALUE;
         for(int i=1;i<prices.length;i++){
-            left[i] = Math.min(prices[i],left[i-1]);
-            dp[i] = Math.max(dp[i-1],prices[i]-left[i-1]);
-            if(max<dp[i]){
-                max = dp[i];
-                //System.out.println(i+":"+max);
-            }
-        }
+            hold[i] = Math.max(hold[i-1],-prices[i]);//더 작으면 새로 삼
+            sold[i] = Math.max(prices[i]+hold[i-1],sold[i-1]);
+            hold2[i] = Math.max(hold2[i-1],sold[i-1]-prices[i]);
+            max = Math.max(sold[i],Math.max(max,hold2[i]+prices[i]));
 
-        //두번돌렸을때의 최댓값도 비교해야 함... 
-        right[prices.length-1] = prices[prices.length-1];//max
-        for(int i=prices.length-2;i>=1;i--){
-            right[i] = Math.max(prices[i],right[i+1]);
-            if(max<dp[i-1]+right[i+1]-prices[i]){
-                max = dp[i-1]+right[i+1]-prices[i];
-                //System.out.println(i+":"+(dp[i-1])+"+"+(right[i+1]-prices[i]));
-            }
+            //System.out.println("i: "+i+" hold:"+hold[i]+" sold:"+sold[i]+" hold2: "+hold2[i]+" max:"+max);
         }
         
         return max;
