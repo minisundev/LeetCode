94e55be381b182834efd14f83758ac476394d8db
Minsun <58019747+minisundev@users.noreply.github.com>
2025-02-15 23:44:06 +0900

    Time: 4 ms (73.76%), Space: 61.3 MB (53.07%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -2,20 +2,20 @@ class Solution {
     public int maxProfit(int[] prices) {
         int max = 0;
         //state 정의 : hold => sold => hold2 => rest
-        int [] hold = new int [prices.length];
-        int [] sold = new int [prices.length];
-        int [] hold2 = new int [prices.length];
+        //int [] hold = new int [prices.length];
+        //int [] sold = new int [prices.length];
+        //int [] hold2 = new int [prices.length];
 
-        hold[0] = -prices[0];//처음에 사버려~~~
-        sold[0] = 0;
-        hold2[0] = Integer.MIN_VALUE;
+        int hold = -prices[0];//처음에 사버려~~~
+        int sold = 0;
+        int hold2 = Integer.MIN_VALUE;
         for(int i=1;i<prices.length;i++){
-            hold[i] = Math.max(hold[i-1],-prices[i]);//더 작으면 새로 삼
-            sold[i] = Math.max(prices[i]+hold[i-1],sold[i-1]);
-            hold2[i] = Math.max(hold2[i-1],sold[i-1]-prices[i]);
-            max = Math.max(sold[i],Math.max(max,hold2[i]+prices[i]));
+            hold = Math.max(hold,-prices[i]);//더 작으면 새로 삼
+            sold = Math.max(prices[i]+hold,sold);
+            hold2 = Math.max(hold2,sold-prices[i]);
+            max = Math.max(sold,Math.max(max,hold2+prices[i]));
 
-            //System.out.println("i: "+i+" hold:"+hold[i]+" sold:"+sold[i]+" hold2: "+hold2[i]+" max:"+max);
+            //System.out.println("i: "+i+" hold:"+hold+" sold:"+sold+" hold2: "+hold2+" max:"+max);
         }
         
         return max;
