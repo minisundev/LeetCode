258bb235493e325fd906eaa8709b251d00b3f2db
Minsun <58019747+minisundev@users.noreply.github.com>
2025-03-05 13:00:30 +0900

    Time: 3 ms (83.46%), Space: 46 MB (32.54%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,45 +1,66 @@
 class Solution {
     public int candy(int[] ratings) {
-        int [] candy = new int [ratings.length];
+        int[] candy = new int[ratings.length];
+        // 등고선이라고 생각해봐
+        // 모노토닉 스택 문제일 수도 있어
+        // 아래처럼 풀면 작 -> 크 들은 다 멀쩡하게 되는데 크 -> 작 들은 몇으로 해야하는지 모르는거야...
+        // 커지면 필요한만큼 - 를 때려버리고
+        // 나중에 최종값은 모든 값을 +로 만들 수 있는 기본값*ratings.length 해서 더해서 돌아가면? 되지 않나?
+        candy[0] = 1;
+
+        for (int i = 1; i < ratings.length; i++) {
+            int current = ratings[i];
+            // 왼쪽과 비교~~~
+            // 왼작 인 경우
+            int next = 1;
+            if (ratings[i - 1] < current) {// 왼쪽이 나보다 더 작은 경우
+                next = candy[i - 1] + 1;
+            }
+
+            // if (current < ratings[i - 1]) {// 왼쪽이 나보다 더 큰 경우
+            //     next = candy[i - 1] - 1;
+            // }
+
+            if (current == ratings[i - 1]) {// 왼쪽이 나랑 같은 경우 1로 내려버리면 최솟값일듯...
+                next = 1;
+            }
+
+            candy[i] = next;
+            //System.out.println(i + ") next:" + next + " total:" + candies);
+            // 어어 모노토닉스택처럼 하면 돼 베이스라인을 싹 다 올릴 필요가 없어 메꿔지는데까지 모노토닉스택에 넣어놧다가 뽑으면 됨?
+        }
+
+        if(candy[ratings.length-1]==0){
+            candy[ratings.length-1] = 1;
+        }
+
+        for (int i = ratings.length-2; i >=0; i--) {
+            int current = ratings[i];
+            // 왼쪽과 비교~~~
+            // 오작 인 경우
+            int next = 1;
+            if (ratings[i + 1] < current) {// 오른쪽이 나보다 더 작은 경우
+                next = candy[i + 1] + 1;
+            }
+
+            // if (current < ratings[i + 1]) {// 오른쪽이 나보다 더 큰 경우
+            //     next = candy[i + 1] - 1;
+            // }
+
+            // if (current == ratings[i - 1]) {// 오른쪽이 나랑 같은 경우 1로 내려버리면 최솟값일듯...
+            //     next = 1;
+            // }
+
+            candy[i] = Math.max(candy[i],next);
+            //System.out.println(i + ") next:" + next + " total:" + candies);
+            // 어어 모노토닉스택처럼 하면 돼 베이스라인을 싹 다 올릴 필요가 없어 메꿔지는데까지 모노토닉스택에 넣어놧다가 뽑으면 됨?
+        }
+
         int candies = 0;
-        
-        //rating이 적은 순서대로 넣어놨다가..그리디로 풀면 될 거 같기도 하고 ㅜ
-        PriorityQueue<Child> pq = new PriorityQueue<>(
-            (o1,o2)->{
-                return o1.rating - o2.rating;
-            }
-        );
 
-        for(int i=0; i<ratings.length; i++){
-            int rating = ratings[i];
-            pq.add(new Child(i,rating));
+        for(int c : candy){
+            candies += c;
         }
-
-        while(!pq.isEmpty()){
-            Child child = pq.poll();
-            //  왼쪽과 비교~~~
-            //  왼작 인 경우
-            int min = 1;
-            if(child.index-1>=0 && ratings[child.index-1]<ratings[child.index]){//왼쪽이 나보다 더 작은 경우
-                min = candy[child.index-1]+1;
-            }
-            //오작 인 경우
-            if(child.index+1<ratings.length && ratings[child.index]>ratings[child.index+1]){//오른쪽이 나보다 더 작은 경우
-                min = Math.max(min,candy[child.index+1]+1);
-            }
-            candy[child.index]=min;
-            candies += min;
-        }
-
         return candies;
-    }
-}
-
-class Child{
-    public int index;
-    public int rating;
-    public Child(int i, int r){
-        index = i;
-        rating = r;
     }
 }
