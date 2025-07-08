2712b8dc91d4e99610e6e5fea7f5b2cfea95ea11
Minsun <58019747+minisundev@users.noreply.github.com>
2025-07-09 00:23:46 +0900

    Time: 9 ms (67.04%), Space: 80.9 MB (50.28%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.java
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -1,55 +1,39 @@
 class Solution {
     public int[] platesBetweenCandles(String s, int[][] queries) {
-        int [] answer = new int [queries.length];
-        //아 누적합 문제인거 같기도 하고요~
-        //일단 *개수 세는 배열이 있고
-        //쿼리 들어오면 왼쪽~오른쪽부터 봐가지고 투포인터 형식으로? |로 둘러싸인 개수를 구하는것임...
-        int [] plates = new int [s.length()];
-        int [] lastCandle = new int [s.length()];
-        int [] nextCandle = new int [s.length()];
+        int n = s.length();
+        int[] answer = new int[queries.length];
 
-        plates[0] = s.charAt(0)=='*' ? 1 : 0;
-        
+        int[] plates = new int[n];
+        int[] lastCandle = new int[n];
+        int[] nextCandle = new int[n];
+
+        // prefix sum
+        plates[0] = s.charAt(0) == '*' ? 1 : 0;
         int last = -1;
+        for (int i = 1; i < n; i++) {
+            plates[i] = plates[i - 1] + (s.charAt(i) == '*' ? 1 : 0);
+            if (s.charAt(i) == '|') last = i;
+            lastCandle[i] = last;
+        }
 
-        for(int i=1; i<s.length(); i++){
-            if(s.charAt(i)=='*'){
-                plates[i] = plates[i-1]+1;
-                lastCandle[i] = last;
-                nextCandle[i] = -1;
-            } else{
-                plates[i] = plates[i-1];
-                if(last!=-1){
-                    nextCandle[last] = i;
-                }
-                last = i;
-                lastCandle[i] = last;
+        int next = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '|') next = i;
+            nextCandle[i] = next;
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int left = queries[i][0];
+            int right = queries[i][1];
+
+            int start = nextCandle[left];
+            int end = lastCandle[right];
+
+            if (start != -1 && end != -1 && start < end) {
+                answer[i] = plates[end] - plates[start];
             }
         }
 
-        for(int i=0; i<queries.length; i++){
-            int [] q = queries[i];
-            int left = q[0];
-            int right = q[1];
-
-            while(s.charAt(left)!='|' && left<right){
-                if(lastCandle[left]!=-1){
-                    left = nextCandle[lastCandle[left]];
-                }else{
-                    left++;
-                }
-                
-            }
-            
-            while(s.charAt(right)!='|' && left<right){
-                right = lastCandle[right];
-            }
-
-            if(left!=right){
-                answer[i] = plates[right]-plates[left];
-            }
-
-        }
         return answer;
     }
 }
