50d0f5604691f925cabc784b8b67589861c7829b
Minsun <58019747+minisundev@users.noreply.github.com>
2025-09-22 09:05:16 +0900

    Time: 4 ms (27.89%), Space: 42.1 MB (6.76%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -1,30 +1,36 @@
 class Solution {
     public String smallestSubsequence(String s) {
-        boolean[] inStack = new boolean[26];
-        int[] lastIndex = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            lastIndex[s.charAt(i) - 'a'] = i;
+        Map<Character,Integer> last = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            last.put(c,i);
         }
 
-        Stack<Character> stack = new Stack<>();
+        Set<Character> contain = new HashSet<>();
+        Deque<Character> dq = new ArrayDeque<>();
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i=0;i<s.length();i++){
+
             char c = s.charAt(i);
-            if (inStack[c - 'a']) continue;
 
-            while (!stack.isEmpty() &&
-                   c < stack.peek() &&
-                   lastIndex[stack.peek() - 'a'] > i) {
-                inStack[stack.pop() - 'a'] = false;
+            if(contain.contains(c)) continue;
+            
+            while(!dq.isEmpty()
+            && dq.peekLast()>c
+            && last.get(dq.peekLast())>i
+            ){
+                char removed = dq.pollLast();
+                contain.remove(removed);
             }
 
-            stack.push(c);
-            inStack[c - 'a'] = true;
+            dq.offerLast(c);
+            contain.add(c);
         }
 
         StringBuilder sb = new StringBuilder();
-        for (char c : stack) sb.append(c);
+        while(!dq.isEmpty()){
+            sb.append(dq.pollFirst());//첫번째부터 뺄수가있어... dq의 승리다..!
+        }
         return sb.toString();
     }
 }
