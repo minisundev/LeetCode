c13f1dfdc07bc1d2b83bb0f8712a94baac518e77
Minsun <58019747+minisundev@users.noreply.github.com>
2025-10-24 14:25:55 +0900

    Time: 5 ms (57.46%), Space: 45 MB (78.58%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,32 +1,34 @@
 class Solution {
+
     public int minDistance(String word1, String word2) {
-        //이것도 대가리 깨지는데 더 빠른 풀이가 있음
         int m = word1.length();
         int n = word2.length();
-
-        //초기화
+        
         int[][] dp = new int[m + 1][n + 1];
-
-        //max값 세팅
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = i;  //word1의 첫 i문자 삭제
+        
+        // 초기화하기
+        for(int i = 0; i <= m; i++){
+            dp[i][0] = i;
         }
-        for (int j = 0; j <= n; j++) {
-            dp[0][j] = j;  //word2의 첫 j문자 삽입
+        for(int j = 0; j <= n; j++){
+            dp[0][j] = j;
         }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];  
+        
+        //dp로 확산시키기
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    //글자 같으면 대각선 그대로(교체 일어나지 않음)
+                    dp[i][j] = dp[i-1][j-1];
                 } else {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1,//대체
-                               Math.min(dp[i][j - 1] + 1,//삽입
-                                        dp[i - 1][j] + 1));//삭제
+                    //글자 다르면: 3가지 중 최소 + 1
+                    int min = Math.min(dp[i][j-1],dp[i-1][j]);//삽입, 삭제 중 최소
+                    min = Math.min(min,dp[i-1][j-1]);//교체
+                    dp[i][j] = min+1;
                 }
             }
         }
-
+        
         return dp[m][n];
     }
 }
