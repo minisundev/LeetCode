ff4ab549a34c633bf8b955b27fab93316304c2f1
Minsun <58019747+minisundev@users.noreply.github.com>
2025-12-16 11:27:53 +0900

    Time: 0 ms (100%), Space: 42.8 MB (63.43%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,63 +1,42 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> list = new ArrayList<>();
 
-        boolean [][] visit = new boolean [matrix.length][matrix[0].length];
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean [][] v = new boolean [n][m];
+
+        int [][] d = {{0,1},{1,0},{0,-1},{-1,0}};
+        int i = 0;
 
         int x = 0;
         int y = 0;
+        list.add(matrix[x][y]);
+        v[0][0]=true;
 
-        int [][] dl = {{0,1},{1,0},{0,-1},{-1,0}};
-        int index = 0;
-
-        list.add(matrix[0][0]);
-        visit[0][0] = true; //들어가서 dl만큼 전진하니까 0,0은 미리 해줘야 함
-        int count = 2; //1을 이미 썼으니 2부터
-
-        while(count<=matrix.length*matrix[0].length){
-
-            int X = x+dl[index][0];
-            int Y = y+dl[index][1];
-
-            if(X<0 || Y<0 || X>=matrix.length || Y>=matrix[0].length || visit[X][Y]){
-                index = (index+1)%dl.length;
+        for(int c=1;c<n*m;c++){
+            
+            int X = x+d[i][0];
+            int Y = y+d[i][1];
+            
+            if(
+                i==0 && (Y>=m||v[X][Y])||
+                i==2 && (Y<0||v[X][Y])||
+                i==1 && (X>=n||v[X][Y])||
+                i==3 && (X<0||v[X][Y])
+            ){
+                i = (i+1)%d.length;
+                c--;
                 continue;
             }
 
-            visit[X][Y] = true;
             list.add(matrix[X][Y]);
+            v[X][Y]=true;
 
-            x = X;
-            y = Y;
-
-            count++;
+            x=X;
+            y=Y;
         }
-
-        // int x = 0;
-        // int y = 0;
-
-        // int [][] d = {{0,1},{1,0},{0,-1},{-1,0}};
-        // int index = 0;
-
-        // list.add(matrix[0][0]);
-        // visit[0][0] = true;
-
-        // for(int i=0; i<matrix.length*matrix[0].length; i++){
-        //     int X = x+d[index][0];
-        //     int Y = y+d[index][1];
-        //     if(X<matrix.length && X>=0 && Y<matrix[0].length && Y>=0 && !visit[X][Y]){
-        //         System.out.println("true");
-        //         visit[X][Y] = true;
-        //         list.add(matrix[X][Y]);
-        //     }else{
-        //         i--;
-        //         index = (index+1)%d.length;
-        //     }
-        //     x = X;
-        //     y = Y;
-        // }
 
         return list;
     }
