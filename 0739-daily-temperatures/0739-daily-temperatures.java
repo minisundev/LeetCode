d9dc1ba2fffdfdf70508983a392d6f7c77e7b1ee
Minsun <58019747+minisundev@users.noreply.github.com>
2026-01-22 09:53:51 +0900

    Time: 23 ms (92.14%), Space: 103.5 MB (87.18%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int[] dailyTemperatures(int[] arr) {
-        //오큰수네.. 다음 더따뜻한 날..s..
-        //없으면 0돌아감
-        Deque<Integer> dq = new ArrayDeque<>();
-        int [] answer = new int[arr.length];
+    public int[] dailyTemperatures(int[] temperatures) {
+        //흠 이거 오큰수 문제잖아
+        int [] answer = new int [temperatures.length];
 
-        for(int i=0; i<arr.length; i++){
-            int temp = arr[i];
-            while(!dq.isEmpty() && arr[dq.peekLast()]<temp){
-                int day = dq.pollLast();
-                answer[day] = i-day;
+        Deque<Integer> d = new ArrayDeque<>();
+
+        for(int i=0; i<temperatures.length; i++){
+            int t = temperatures[i];
+            while(!d.isEmpty() && temperatures[d.peekLast()]<t){
+                int index = d.pollLast();
+                answer[index] = i-index;
             }
-            dq.offerLast(i);
+            d.offerLast(i);
         }
 
         return answer;
