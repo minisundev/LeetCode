02bb52b7fdc2619ae7d6f69c4fa6b0364a25562e
Minsun <58019747+minisundev@users.noreply.github.com>
2026-02-15 16:50:53 +0900

    Time: 1 ms (99.06%), Space: 42.7 MB (50.98%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,35 +1,21 @@
 class Solution {
     public boolean isUgly(int n) {
-        //2로 나눠질때까지 나누고 
-        //3으로 나눠질때까지 나누고
-        //5로 나눠질때까지 나누고 -> 해서 마지막이 1인지 보면 되는 문제인 거 아냐...?
-        while(n>1){
-            if(n%2==0){
-                n=n/2;
-            }else{
-                break;
-            }
-            System.out.println(n);
+        //2와 3과 5로만 이루어졌다면 ugly인데 7이 포함되거나 다른 소수가 포함되면 안됨
+        //계속 나눠가지고 2랑 3이랑 5로 계속 나눠가지고 마지막에 남는게 1이 아니면 다른게 포함된거겠지...
+        //2로 나눠
+        if(n==0) return false;
+        while(n%2==0){
+            n = n/2;
         }
-        while(n>1){
-            if(n%3==0){
-                n=n/3;
-            }else{
-                break;
-            }
-            System.out.println(n);
+        //3으로 나눠
+        while(n%3==0){
+            n = n/3;
         }
-        while(n>1){
-            if(n%5==0){
-                n=n/5;
-            }else{
-                break;
-            }
-            System.out.println(n);
+        //5로 나눠
+        while(n%5==0){
+            n = n/5;
         }
-        if(n==1){
-            return true;
-        }
+        if(n==1) return true;
         return false;
     }
 }
