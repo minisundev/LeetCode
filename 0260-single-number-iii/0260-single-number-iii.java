9cd6addfe68c03adcab27f8f0d2a16950ccecccc
Minsun <58019747+minisundev@users.noreply.github.com>
2026-04-21 23:31:45 +0900

    Time: 4 ms (37.19%), Space: 48 MB (43.75%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
+        //졸려죽어요
+        //2개는 한번만 나타나고
+        //다른것들은 2번씩 나타난대
+        //1번씩만 나오는거 2개 찾으래 그럼 해쉬겟지 뭐
+        //linear runtime complexity
+        //only constant extra space
+        //해쉬쓰래
         Set<Integer> set = new HashSet<>();
-        for(int i : nums){
-            if(!set.contains(i)){
-                set.add(i);
-            }else{
+        for(int i: nums){
+            if(set.contains(i)){
                 set.remove(i);
+            }else{
+                set.add(i);
             }
         }
-        int [] answer = new int [2];
-        int index = 0;
-        for(int i : new ArrayList<>(set)){
-            answer[index++] = i;
-        }
-        return answer;
+        List<Integer> list = new ArrayList<>(set);
+        int [] arr = {list.get(0), list.get(1)};
+        return arr;
     }
 }
