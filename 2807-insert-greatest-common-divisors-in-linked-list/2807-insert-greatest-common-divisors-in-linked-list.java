259637a7ba453037335dd8689c546c7421572f32
Minsun <58019747+minisundev@users.noreply.github.com>
2026-06-17 13:59:23 +0900

    Time: 2 ms (44.1%), Space: 47.3 MB (28.54%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.java
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.java
@@ -21,13 +21,33 @@ class Solution {
     }
 
     int getGCD(int a, int b){
-        int max = Math.max(a,b);
-        int min = Math.min(a,b);
-        //이것도 recursion일것같은데~
-        int newMax = max - min;
-        if (newMax==0){
-            return max;
+        //유클리드 호제법~
+
+        //큰 수에서 작은 수를 빼도 최대공약수는 변하지 않는다
+        //A = G * a
+        //B = G * b
+        //A - B = (G * a) - (G * b)
+        //A - B = G * (a - b)
+        // int max = Math.max(a,b);
+        // int min = Math.min(a,b);
+        // //이것도 recursion일것같은데~
+        // int newMax = max - min;
+        // if (newMax==0){
+        //     return max;
+        // }
+        // return getGCD(newMax,min);
+
+        //나머지 연산도 마찬가지
+        //A % B = A - (q * B)
+        //= (G * a) - q * (G * b)
+        //= (G * a) - (G * q * b)
+        //= G * (a - q * b)
+        
+        // b가 0이 되면 남은 a가 최대공약수
+        if (b == 0) {
+            return a;
         }
-        return getGCD(newMax,min);
+        // a를 b로 나눈 나머지를 구해서 다시 재귀 호출
+        return getGCD(b, a % b);
     }
 }
