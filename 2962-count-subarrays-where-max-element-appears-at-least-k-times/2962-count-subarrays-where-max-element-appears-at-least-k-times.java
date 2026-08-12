82ee3b766d4423a140e62c4b8cba41a000383854
Minsun <58019747+minisundev@users.noreply.github.com>
2026-08-12 14:51:36 +0900

    Time: 78 ms (5.05%), Space: 173 MB (5.05%) - LeetHub

---
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -0,0 +1,52 @@
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        // 흠 이것도 사실상 진심 똑같은 문제네...
+        long answer = 0;
+
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        int max = nums[0];
+
+        for(int i=0; i<nums.length; i++){
+            int n = nums[i];
+            max = Math.max(n,max);
+
+            List<Integer> list = map.getOrDefault(n, new ArrayList<>());
+            list.add(i);
+            map.put(n,list);
+        }
+
+        List<Integer> index = map.get(max);
+
+        if(index.size() < k){ return 0L; }
+
+        int maximum = index.size()-(k-1);
+
+        for(int i=0; i<maximum; i++){
+            int leftIdx = index.get(i);
+            int rightIdx = index.get(i+k-1);
+            int rightIdx2 = nums.length;
+            if(i!=maximum-1){
+                rightIdx2 = index.get(i+k);
+            }
+            
+
+            // 앞쪽은 다 포함
+            long leftNumber = leftIdx;
+
+            // 맨 마지막인 것 처리 -> 얘만 조합이 생겨서 곱해주면 됨
+            long rightNumber  = rightIdx2 - rightIdx -1;
+
+            // 그래서 조합할 수 있는 것의 길이가... 일단 k개는 포함이니까 본통은 다 포함이고
+            // 앞으로만 세어야 중복이 없음~
+
+            long number = (leftNumber+1) * (rightNumber+1);
+            answer += number;
+
+            //System.out.print(leftIdx +"-"+rightIdx+ " -> ");
+            //System.out.println(leftNumber +","+rightNumber+ " number:"+number+" answer:"+answer);
+        }
+
+        
+        return answer;
+    }
+}
